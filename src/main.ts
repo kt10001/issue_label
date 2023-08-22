d6ae50a0293a0434b6540e4cbe011480326be176
kt <13093323120@163.com>
2023-08-22 06:40:45 +0000

    add username

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import * as core from '@actions/core'
+import * as core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    core.info(`Hello world`)
+    core.info(`Hello world`);
+    const username = core.getInput("user_name");
+    core.info(`Hello ${username}`);
+
+    core.info(`username === admin : ${username === "admin"}`);
   } catch (error) {
-    core.info(`error`)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
